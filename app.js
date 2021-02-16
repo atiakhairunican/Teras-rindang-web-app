e7924f2be337921d71b10ecdf1c021d4202bfa78
atiakhairunican <atiakhairunican@gmail.com>
2021-02-16 10:47:32 +0700

    change url

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ server.use(bodyPars.json())
 server.use(morgan("dev"))
 server.use(cors())
 server.use("/public", express.static("public"))
-server.use("/api", router)
+server.use("/terasApi", router)
 
 redis.redisCheck()
     .then((res) => {
